0ba3b38451aec73d10bf68532f00ad63e5d83162
Marcley <marcleyrosa@gmail.com>
2022-05-23 09:37:30 -0300

    Requisito 11 Bônus

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,31 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(telNumber) {
+  bole = false
+  let result = '(' + telNumber[0] + telNumber[1] + ') ' + telNumber[2] + telNumber[3] + telNumber[4] + telNumber[5] + telNumber[6] + '-' + telNumber[7] + telNumber[8] + telNumber[9] + telNumber[10];
+  if (telNumber.length !== 11) {
+    return 'Array com tamanho incorreto.'
+  }
+  for (let n of telNumber) {
+    let repet = 0
+      for (let ind = 0; ind < telNumber.length; ind += 1) {
+        if (telNumber[ind] > 9 || telNumber[ind] < 0) {
+          result = 'não é possível gerar um número de telefone com esses valores'
+        } 
+        if (n === telNumber[ind]) {
+          repet = repet + 1
+          if (repet > 2) {
+            result = 'não é possível gerar um número de telefone com esses valores'
+          } else {
+            bole = true
+          }
+      }
+      }
+    
+  }
+
+  if (bole === true) {
+    return result
+  }
 }
 
 // Desafio 12

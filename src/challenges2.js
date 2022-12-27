82e1b4368be03ef5fdb8b4beabd5ab90ad3f1078
Marcley <marcleyrosa@gmail.com>
2022-12-27 08:43:04 -0300

    Requisito 13 refatorado

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,42 +1,26 @@
 // Desafio 11
 
-function numGenerate(telNumber, comp, num, message) {
-  let repet = 0;
-  for (let ind = 0; ind < telNumber.length; ind += 1) {
-    if (comp) {
-      return message;
-    }
-    if (num === telNumber[ind]) {
-      repet += 1;
-      if (repet > 2) {
-        return message;
-      }
-    }
-  }
-}
+// const numGenerate = (repet, message) => {
+//   if (repet > 2) return message;
+// };
 
 function generatePhoneNumber(telNumber) {
-  const message = 'não é possível gerar um número de telefone com esses valores'
+  const message = 'não é possível gerar um número de telefone com esses valores';
   if (telNumber.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
   for (let num of telNumber) {
+    if (num > 9 || num < 0) return message;
     let repet = 0;
     for (let ind = 0; ind < telNumber.length; ind += 1) {
-      if (num > 9 || num < 0) {
-        return message
-      }
       if (num === telNumber[ind]) {
         repet += 1;
-        if (repet > 2) {
-          return message
-        }
+        if (repet > 2) return message;
       }
     }
   }
   const n = telNumber;
   const result = `(${n[0]}${n[1]}) ${n[2]}${n[3]}${n[4]}${n[5]}${n[6]}-${n[7]}${n[8]}${n[9]}${n[10]}`;
-
   return result;
 }
 
@@ -57,9 +41,20 @@ function triangleCheck(lineA, lineB, lineC) {
   return false;
 }
 
-// Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(frase) {
+  let glass = 0;
+  const test = frase.split(' ');
+  for (let num of test) {
+    const numbers = Number(num);
+    if (!Number.isNaN(numbers)) {
+      glass += numbers;
+    }
+  }
+  let plur = 'copos';
+  if (glass === 1) {
+    plur = 'copo';
+  }
+  return `${glass} ${plur} de água`;
 }
 
 module.exports = {

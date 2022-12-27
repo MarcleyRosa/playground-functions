c076e53f9e23c611a7bf8250b1fee4df589dc99e
Marcley <marcleyrosa@gmail.com>
2022-12-27 10:06:45 -0300

    Correções de lint e complexidade

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,30 +1,44 @@
 // Desafio 11
 
-// const numGenerate = (repet, message) => {
-//   if (repet > 2) return message;
-// };
+// for (let ind = 0; ind < telNumber.length; ind += 1) {
+//   if (num === telNumber[ind]) {
+//     repet += 1;
+//     if (repet > 2) return message;
+//   }
+// }
+
+// function counts(countMap, num, message) {
+//   countMap[num] = (countMap[num] || 0) + 1;
+//   if (countMap[num] === 3) return message;
+// }
+
+function compare(param1, param2) {
+  return param1 || param2;
+}
 
 function generatePhoneNumber(telNumber) {
   const message = 'não é possível gerar um número de telefone com esses valores';
   if (telNumber.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
+  const countMap = [];
   for (let num of telNumber) {
-    if (num > 9 || num < 0) return message;
-    let repet = 0;
-    for (let ind = 0; ind < telNumber.length; ind += 1) {
-      if (num === telNumber[ind]) {
-        repet += 1;
-        if (repet > 2) return message;
-      }
-    }
+    countMap[num] = (compare(countMap[num], 0)) + 1;
+    if (compare(num > 9, num < 0) || countMap[num] === 3) return message;
   }
   const n = telNumber;
-  const result = `(${n[0]}${n[1]}) ${n[2]}${n[3]}${n[4]}${n[5]}${n[6]}-${n[7]}${n[8]}${n[9]}${n[10]}`;
-  return result;
+  const res = `(${n[0]}${n[1]}) ${n[2]}${n[3]}${n[4]}${n[5]}${n[6]}-${n[7]}${n[8]}${n[9]}${n[10]}`;
+  return res;
 }
 
 // Desafio 12
+
+function testss(param1, param2, param3) {
+  if (param1 && param2 && param3) {
+    return true;
+  }
+  return false;
+}
 function triangleCheck(lineA, lineB, lineC) {
   let ab = lineA + lineB;
   let ac = lineA + lineC;
@@ -32,11 +46,11 @@ function triangleCheck(lineA, lineB, lineC) {
   let AB = Math.abs(lineA - lineB);
   let AC = Math.abs(lineA - lineC);
   let BC = Math.abs(lineB - lineC);
+  const lineAC = lineB > AC;
+  const lineBC = lineA > BC;
+  const lineAB = lineC > AB;
   if (lineA < ab && lineB < ac && lineC < bc) {
-    if (lineB > AC && lineA > BC && lineC > AB) {
-      return true;
-    }
-    return false;
+    return testss(lineAC, lineBC, lineAB);
   }
   return false;
 }
